05cbf92082a6554beb3295af1a06aaccd193788a
Miguh Ruiz <miguhruiz@gmail.com>
2016-08-26 18:45:47 +0200

    [Add] User can enter data

--- helloworld.rb
+++ helloworld.rb
@@ -3,9 +3,12 @@
 puts "Hola Miguh :D"
 
 def wellcome_to_course
-  nombre = "Miguh"
-  apellido = "Ruiz"
-  denominacion = "Don"
+  puts "¿Cómo te llamas?"
+  nombre = gets.to_s
+  puts "¿Y tu apellido era...?"
+  apellido = gets.to_s
+  puts "Finalmente, ¿cómo te gusta que te llamen? ¿Don, Señor...?"
+  denominacion = gets.to_s
   primera_clase = "20 de Septiembre 20:00PM"
   puts "Bienvenido a el curso #{denominacion} #{nombre} #{apellido}. Es un placer tenerle aquí. La primera clase es el #{primera_clase}"
 end
