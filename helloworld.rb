ecaae3fc9d888740a92ad476a0ae81a1f9fdb311
Miguh Ruiz <miguhruiz@gmail.com>
2016-08-26 18:39:03 +0200

    [Add] Hello World

---
+++ helloworld.rb
@@ -0,0 +1,13 @@
+# Hello World en Ruby
+
+puts "Hola Miguh :D"
+
+def wellcome_to_course
+  nombre = "Miguh"
+  apellido = "Ruiz"
+  denominacion = "Don"
+  primera_clase = "20 de Septiembre 20:00PM"
+  puts "Bienvenido a el curso #{denominacion} #{nombre} #{apellido}. Es un placer tenerle aquí. La primera clase es el #{primera_clase}"
+end
+
+wellcome_to_course()
